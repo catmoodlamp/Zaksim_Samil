84b0df456a7a6567ead0e2802a17e1a1a7018e18
고양이 무드 등 <catmoodlamp@gmail.com>
2021-03-10 05:50:51 +0900

    죄송합니다 테슷흐였습니다

---
+++ 6장-메소드로 코드 간추리기/메모페도/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 메모페도
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 6장-메소드로 코드 간추리기/엑스트라 유틸리티2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+namespace 엑스트라_유틸리티2
+{
+    class EU
+    {
+        public static void Swap<T>(ref T a, ref T b) where T:struct
+        {
+            T tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float hodu = 3.14f;
+            float klee = 69.74f;
+            WriteLine($"스와핑 전 : 호두:{hodu}, 클레:{klee}");
+            EU.Swap(ref hodu, ref klee);
+            WriteLine($"스와핑 후 : 호두:{hodu}, 클레:{klee}");
+        }
+    }
+}
